870c24c38cf1da96459ed41e8d8db15dd31d508e
Jtmy99 <37224111+Jtmy99@users.noreply.github.com>
2018-03-09 18:02:42 -0800

    Create sorting.cpp

---
+++ sorting.cpp
@@ -0,0 +1,9 @@
+//This is the file to help to sort through all the data that is sent in
+
+#include <iostream>
+#include <fstream>
+
+int main(void){
+	cout << "This is a test to see if this thing works"
+	return 0;
+}
